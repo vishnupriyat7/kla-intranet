6345bc31a8d0a1b3b4ea9cebecabb79c77f48e15
vishnupriya.t7@gmail.com <vishnupriya.t7@gmail.com>
2025-02-17 12:18:38 +0530

    periodicals/order-circular create blade changed

--- resources/views/newsupdates/index.blade.php
+++ resources/views/newsupdates/index.blade.php
@@ -1,25 +1,4 @@
 <x-app-layout>
-    {{-- <div class="container mt-5">
-        <h2 class="mb-4">News List</h2>
-        <div class="d-flex justify-content-end mb-4">
-            <a href="{{ route('news-updates.create') }}" class="btn btn-primary">Add News</a>
-        </div>
-        <table class="table table-striped table-bordered">
-            <thead class="table-dark">
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Date</th>
-                    <th>Status</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-
-
-        </table>
-
-    </div> --}}
-
     <div class="container mt-3">
         {{-- add card here --}}
         <div class="card ">
--- resources/views/periodicals/create.blade.php
+++ resources/views/periodicals/create.blade.php
@@ -5,60 +5,60 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+    <div class="container py-12">
+        <div class="card">
+            <div class="card-header">
+                <label for="name_eng" class="form-label">Periodical Master</label>
+            </div>
+            <div class="card-body">
 
+                <form action="{{ route('periodicals.store') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
 
-                    <form action="{{ route('periodicals.store') }}" method="POST" enctype="multipart/form-data">
-                        @csrf
+                    <div class="mb-3">
+                        <label for="periodical_item" class="form-label">Periodical Item</label>
+                        <select class="form-select" id="name_eng" name="name_eng" required>
+                            <option value="">Select Periodical Item</option>
+                            @foreach ($periodicalMasters as $periodicalMaster)
+                                <option value="{{ $periodicalMaster->id }}">{{ $periodicalMaster->name }}</option>
+                            @endforeach
+                        </select>
+                        @error('name_eng')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                        <div class="mb-3">
-                            <label for="name_eng" class="form-label">Periodical Master</label>
-                            <select class="form-select" id="name_eng" name="name_eng" required>
-                                <option value="">Select Periodical Item</option>
-                                @foreach ($periodicalMasters as $periodicalMaster)
-                                    <option value="{{ $periodicalMaster->id }}">{{ $periodicalMaster->name }}</option>
-                                @endforeach
-                            </select>
-                            @error('name_eng')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
+                    <div class="mb-3">
+                        <label for="date" class="form-label">Date</label>
+                        <input type="date" class="form-control" id="date" name="date" required>
+                        @error('date')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="path" class="form-label">Upload File (Path)</label>
+                        <input type="file" class="form-control" id="path" name="path" required>
+                        @error('path')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    {{-- Add Select Box for Periodical List Published(1) /Unpublished(0) --}}
+                    <div class="mb-3">
+                        <label for="status" class="form-label">Published / Unpublished</label>
+                        <select class="form-select" id="status" name="status" required>
+                            <option value="">Select Published / Unpublished</option>
+                            <option value="1">Published</option>
+                            <option value="0">Unpublished</option>
+                        </select>
+                        @error('status')
+                            <div class="text-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
 
-                        <div class="mb-3">
-                            <label for="date" class="form-label">Date</label>
-                            <input type="date" class="form-control" id="date" name="date" required>
-                            @error('date')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="path" class="form-label">Upload File (Path)</label>
-                            <input type="file" class="form-control" id="path" name="path" required>
-                            @error('path')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
-                        {{-- Add Select Box for Periodical List Published(1) /Unpublished(0) --}}
-                        <div class="mb-3">
-                            <label for="status" class="form-label">Published / Unpublished</label>
-                            <select class="form-select" id="status" name="status" required>
-                                <option value="">Select Published / Unpublished</option>
-                                <option value="1">Published</option>
-                                <option value="0">Unpublished</option>
-                            </select>
-                            @error('status')
-                                <div class="text-danger">{{ $message }}</div>
-                            @enderror
-                        </div>
+                    <button type="submit" class="btn btn-success">Submit</button>
+                    <a href="{{ route('periodicals.index') }}" class="btn btn-secondary">Back</a>
+                </form>
 
-                        <button type="submit" class="btn btn-success">Submit</button>
-                        <a href="{{ route('periodicals.index') }}" class="btn btn-secondary">Back</a>
-                    </form>
-
-                </div>
             </div>
         </div>
     </div>
